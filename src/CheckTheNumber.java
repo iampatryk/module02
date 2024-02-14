8d3d5fabccabee69d793b25406f67b95eb0ec9bf
iampatryk <mpatrykmiller@gmail.com>
2024-02-14 17:54:49 +0100

    First task

--- src/CheckTheNumber.java
+++ src/CheckTheNumber.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 
-public class GetNumber {
-    public GetNumber(int num) {
-
-    }
+public class CheckTheNumber {
 
     public static void getNumberFromUser() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int num = scanner.nextInt();
 
-        GetNumber newNumber = new GetNumber(num);
-
-
-        if ( newNumber < 0){
-            System.out.println(num + "jest wieksze ");
+        if ( num > 0){
+            System.out.println("the number is greater than 0");
+        } else if (num < 0) {
+            System.out.println("the number is smaller than 0");
+        } else {
+            System.out.println("the number is equal 0");
         }
     }
 }
