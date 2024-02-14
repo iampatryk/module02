82467829b3bcfb53fc0c8669fd93cdeff9276208
iampatryk <mpatrykmiller@gmail.com>
2024-02-14 17:56:29 +0100

    First task

--- src/CheckTheNumber.java
+++ src/CheckTheNumber.java
@@ -8,11 +8,11 @@ public class CheckTheNumber {
         int num = scanner.nextInt();
 
         if ( num > 0){
-            System.out.println("the number is greater than 0");
+            System.out.println("the number is greater than 0.");
         } else if (num < 0) {
-            System.out.println("the number is smaller than 0");
+            System.out.println("the number is smaller than 0.");
         } else {
-            System.out.println("the number is equal 0");
+            System.out.println("the number is equal 0.");
         }
     }
 }
