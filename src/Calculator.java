0fb0dc028406fff9aee17d8c98c3ebae7e35aac9
iampatryk <mpatrykmiller@gmail.com>
2024-02-19 17:35:20 +0100

    update 19/02

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,39 +2,88 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public void start() {
+    public static void start() {
         System.out.println("Hello in the Calculator!");
         System.out.println("You can choose different operators: +, -, *, / ");
-        System.out.println("If you want to leave, enter: exit or quit.");
+        System.out.println("If you want to leave, enter: exit .");
+
+        while(true) {
+            int firstNumber = getFirstNumberFromUser();
+            int secondNumber =getSecondNumberFromUser();
+            String operation = getOperationFromUser();
+            int result = 0;
+
+            switch (operation) {
+                case "+":
+                    result = add(firstNumber, secondNumber);
+                    break;
+                case "-":
+                    result = subtract(firstNumber, secondNumber);
+                    break;
+                case "*":
+                    result = multiply(firstNumber, secondNumber);
+                    break;
+                case "/":
+                    if (isMoreThan0(secondNumber)){
+                        result = divide(firstNumber, secondNumber);
+                    } else {
+                        System.out.println("You can't divide by 0");
+                        continue;
+                    }
+                    break;
+                default:
+                    System.out.println("Error, try again");
+                    continue;
+            }
+            System.out.println("Result: " + " " + firstNumber + " " + operation + " " + secondNumber + " = " + result);
+            System.out.println("Do you want to continue? Type 'yes' to continue. If not, type 'exit', 'quit' or 'no' ."); {
+                Scanner scanner = new Scanner(System.in);
+                String continueCommand = scanner.next();
+                if (continueCommand.equals("no") || continueCommand.equals("exit") || continueCommand.equals("quit")) {
+                    break;
+                }
+            }
+        }
     }
 
-    private String getOperationFromUser() {
+    private static String getOperationFromUser() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter you operation +, -, *, / : ");
         return scanner.nextLine();
     }
 
-    private int getNumberFromUser() {
-        return 0;
+    private static int getFirstNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first number: ");
+        return scanner.nextInt();
+    }
+    private static int getSecondNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your second number: ");
+        return scanner.nextInt();
     }
 
-    private boolean isMoreThan0(int number) {
+    private static boolean isMoreThan0(int number) {
+
         return number > 0;
     }
 
-    private int add(int a, int b) {
+    private static int add(int a, int b) {
+
         return (int) (a + b);
     }
 
-    private int subtract(int a, int b) {
+    private static int subtract(int a, int b) {
         return (int) (a - b);
     }
 
-    private int divide(int a, int b) {
+    private static int divide(int a, int b) {
+
         return (int) (a / b);
     }
 
-    private int multiply(int a, int b) {
+    private static int multiply(int a, int b) {
+
         return (int) (a * b);
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!" + "\n" + "Zadania: ");
+        System.out.println("Hello world!" + "\n" + "Zadania: " + "\n");
 //        CheckTheNumber.getNumberFromUser();
 //        AvarageThreeNum.getThreeNumbers();
 //        SmallestNumber.getSmallestNumber();
@@ -11,6 +11,7 @@ public class Main {
 //        Factorial.getFactorialNumber();
 //        CheckPalindrom.checker();
 //        PrintTriangle.getNumberFromUsers();
+        Calculator.start();
 
     }
 }
